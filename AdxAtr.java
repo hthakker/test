ba7816f0618bf2cf896d0eaa6fa80a83356912b3
hthakker <hardik1910@gmail.com>
2014-05-01 10:47:37 +0530

    updated from editor

--- AdxAtr.java
+++ AdxAtr.java
@@ -4,16 +4,12 @@ import com.breezetrader.Event;
 import com.breezetrader.OrderType;
 import com.breezetrader.Strategy;
 import com.breezetrader.Tick;
-/*
-*  This is a sample strategy intended to get you started.
-*  For more details on the APIs and usage please visit 
-*  http://docs.breezetrader.com
-*
-*/
+
+
 public class AdxAtr extends Strategy {
 	
 	boolean flag = false;
-	String symbol = "EUR.USD";
+	String symbol = "HINDUNILVR";
 	/*
 	*  initialize your context, 
 	*  technical indicators other variables
@@ -22,15 +18,14 @@ public class AdxAtr extends Strategy {
 	{	
 			initTALib("macd","macd1", "12", "26","9", "EU0009652759", "currentValueDouble" );
 			initTALib("rsi","rsi1", "12", "EU0009652759", "currentValueDouble" );
-			context.setDataFrequency(2, Context.Frequency.WEEK);
-			context.setSymbols("EU0009652759");
-			context.setDataURL("https://s3-ap-southeast-1.amazonaws.com/breezetrader-test/EU0009652759.csv");
+	
+			context.setDataFrequency(2, Context.Frequency.DAY);
+			context.setSymbols(symbol);
 			context.setPortfolioValue(BigDecimal.valueOf(100000));
 			context.setDataType(Event.Type.BAR);
 			context.setStartDate("03-9-2012");
 			context.setEndDate("31-10-2012");
 			log("Initialized");
-			
 	}
 	
 	/*
@@ -39,8 +34,7 @@ public class AdxAtr extends Strategy {
 	*  object you intialized in intialize(Context context) 
 	*/
 	
-	public void onEvent(Object object)
-	{
+	public void onEvent(Object object) {
 		double macd1 = getData("macd1" , "macd");
 		double macd1Hist = getData("macd1" , "macdhist");
 		double rsi1 = getData("rsi1");
@@ -48,26 +42,5 @@ public class AdxAtr extends Strategy {
 //		log("macd1hist: "+macd1Hist);
 //		log("macd1sig: "+macd1Sig);
 		log("current Time: "+getTimeStamp());
-		if(rsi1>70)
-			log("rsi > 70");
-		else if(rsi1<30)
-			log("rsi < 30");
-		else
-			log("rsi = "+rsi1);
-		if(macd1 > macd1Hist)
-		{
-			log("ID: "+order(OrderType.Market,symbol, 10));
-			
-			log("openpositions: "+getPosition(symbol));
-		}
-		else
-		{
-			log("openpositions: "+getPosition(symbol));
-			closeAllPositions(symbol);
-			
-		}
-		
-	}
-	
-}
+    }
 	
