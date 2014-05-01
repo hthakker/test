c2d3f7863670bf5c20c89ddcaa2b47d1fa56471d
hthakker <hardik1910@gmail.com>
2014-05-01 10:56:38 +0530

    updated from editor

--- AdxAtr.java
+++ AdxAtr.java
@@ -5,7 +5,6 @@ import com.breezetrader.OrderType;
 import com.breezetrader.Strategy;
 import com.breezetrader.Tick;
 
-
 public class AdxAtr extends Strategy {
 	
 	boolean flag = false;
@@ -16,9 +15,7 @@ public class AdxAtr extends Strategy {
 	*/
 	public void initialize(Context context)
 	{	
-			initTALib("macd","macd1", "12", "26","9", "EU0009652759", "currentValueDouble" );
-			initTALib("rsi","rsi1", "12", "EU0009652759", "currentValueDouble" );
-	
+			initTALib("adx","adx", "14", "9", symbol, close );
 			context.setDataFrequency(2, Context.Frequency.DAY);
 			context.setSymbols(symbol);
 			context.setPortfolioValue(BigDecimal.valueOf(100000));
@@ -35,10 +32,9 @@ public class AdxAtr extends Strategy {
 	*/
 	
 	public void onEvent(Object object) {
-		double macd1 = getData("macd1" , "macd");
-		double macd1Hist = getData("macd1" , "macdhist");
+		double adx = getData("adx");
 		double rsi1 = getData("rsi1");
-//		log("macd1: "+macd1);
+		log("ADX: "+adx + "\n");
 //		log("macd1hist: "+macd1Hist);
 //		log("macd1sig: "+macd1Sig);
 		log("current Time: "+getTimeStamp());
