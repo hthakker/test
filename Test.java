786444260c1595f4b6b059da77dea8581d460e54
hthakker <hardik1910@gmail.com>
2014-05-01 11:10:46 +0530

    test commit

---
+++ Test.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import com.breezetrader.Context;
+import com.breezetrader.Event;
+import com.breezetrader.OrderType;
+import com.breezetrader.Strategy;
+import com.breezetrader.Tick;
+
+public class Test extends Strategy {
+	
+	boolean flag = false;
+	String symbol = "HINDUNILVR";
+	/*
+	*  initialize your context, 
+	*  technical indicators other variables
+	*/
+	public void initialize(Context context)	{	
+
+	//		initTALib("adx","adx", "14", "9", symbol, "close" );
+			initTALib("rsi","rsi14", "14", symbol, "close" );
+			context.setDataFrequency(1, Context.Frequency.DAY);
+			context.setSymbols(symbol);
+			context.setPortfolioValue(BigDecimal.valueOf(100000));
+			context.setDataType(Event.Type.BAR);
+			context.setStartDate("01-01-2013");
+			context.setEndDate("31-12-2013");
+			log("Initialized");
+			double rsi14 = getData("rsi14");
+		    log("RSI14: "+rsi14 + "\n");
+	}
+	
+}
